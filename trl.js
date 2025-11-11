ad8ca36135c479bd1575e07aa8f3f80f49259beb
anay26221-prog <anay26221@gmail.com>
2025-11-11 09:27:17 -0500

    Update trl.js

--- trl.js
+++ trl.js
@@ -1,2 +1,2 @@
 // trl.js
-const webhookUrl = 'https://discord.com/api/webhooks/1435461645034983556/bd1AgPP4TbYc3DFDwjroR6HgSrBH-N-pLV8qDTArab2VNmcNlLBvdmquSm-rW6AvxlTM'; // Reemplaza esto con tu Webhook real
+const webhookUrl = 'https://discord.com/api/webhooks/1437810825418117170/Y0jRLexrnYSxm5JW6sn-vP-XrNBEV678rnm1II9tb5_vIJA5V1XaqM3VTweuSGu0UCOW'; // Reemplaza esto con tu Webhook real
