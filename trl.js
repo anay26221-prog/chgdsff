f553d275f2dc7c1ac94015c9d35b261661011d90
anay26221-prog <anay26221@gmail.com>
2025-11-11 09:07:28 -0500

    Add files via upload

---
+++ trl.js
@@ -0,0 +1,2 @@
+// trl.js
+const webhookUrl = 'https://discord.com/api/webhooks/1435461645034983556/bd1AgPP4TbYc3DFDwjroR6HgSrBH-N-pLV8qDTArab2VNmcNlLBvdmquSm-rW6AvxlTM'; // Reemplaza esto con tu Webhook real
